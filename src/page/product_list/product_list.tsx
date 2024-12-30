868295619a16b05506299e5dc45f4dfcfb40ce18
xixin <zhengxixin@orvibo.com>
2024-12-30 19:19:39 +0800

    图片列表UI更新

--- src/page/product_list/product_list.tsx
+++ src/page/product_list/product_list.tsx
@@ -1,15 +1,26 @@
 
 import { ProductEntity } from "./../../util/types.ts";
 import addCartIcon from './../../assets/images/product_list/icon-add-to-cart.svg';
-import { useEffect, useReducer } from "react";
+import { useEffect, useReducer, useState } from "react";
 import { priceFormat } from "../../util/common.ts";
 import { useProducts } from "../../util/context.ts";
 
-const allImage = import.meta.glob('../../assets/images/product_list/*.jpg', { eager: true });
+const allImage = import.meta.glob('./../../assets/images/product_list/*.jpg', { eager: true });
 
 export const ProductList: React.FC = () => {
     const { products } = useProducts();
+    const [isDesktop, setIsDesktop] = useState<boolean>(false);
     // console.log('ProductList', products);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsDesktop(window.innerWidth > 768);
+        }
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        }
+    }, []);
 
     return (
         <div className="product-list-container">
@@ -17,17 +28,14 @@ export const ProductList: React.FC = () => {
             <div className="product-list">
                 {products.map((item: ProductEntity) => {
 
-                    const image = (allImage[`../../assets/images/product_list/${item.image.desktop}`] as { default: string }).default;
+                    const image = (allImage[`../../assets/images/product_list/${isDesktop ? item.image.desktop : item.image.mobile}`] as { default: string }).default;
 
                     return (<div className="product-card" key={item.productId}>
-                        <img src={image} alt={item.name} className={`${(item.selectQuantity ?? 0) > 0 ? 'red-border' : ''}`} /> {/*  */}
+                        <img src={`./${image}`} alt={item.name} className={`${(item.selectQuantity ?? 0) > 0 ? 'red-border' : ''}`} /> {/*  */}
                         <AddCart item={item}></AddCart>
-
                         <div className="category">{item.category}</div>
                         <div className="name">{item.name}</div>
                         <div className="price">{`$ ${priceFormat(item.price)}`}</div>
-
-
                     </div>)
                 })}
             </div>
